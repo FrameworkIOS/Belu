1c1f419fd5e71b6cacd2a5a9cf1125ea936c90a7
krishpranav <krisna.pranav@gmail.com>
2023-07-09 17:38:36 +0530

    Belu: Communicable

--- Belu/Communicable.swift
+++ Belu/Communicable.swift
@@ -9,19 +9,22 @@ import Foundation
 import CoreBluetooth
 
 public enum RequestMethod {
+
     case get(isNotified: Bool)
     case post
     case broadcast(isNotified: Bool)
-    
+
     var properties: CBCharacteristicProperties {
         switch self {
-        case .get(let isNotifiy):
-            if isNotifiy {
+        case .get(let isNotify):
+            if isNotify {
                 return [.read, .notify]
             }
             return .read
+            
         case .post:
             return .write
+            
         case .broadcast(let isNotify):
             if isNotify {
                 return [.notify, .broadcast]
@@ -29,35 +32,46 @@ public enum RequestMethod {
             return [.broadcast]
         }
     }
-}
 
+    var permissions: CBAttributePermissions {
+        switch self {
+        case .get: return .readable
+        case .post: return .writeable
+        case .broadcast: return .readable
+        }
+    }
+}
 
 public protocol Communicable: Hashable {
     var serviceUUID: CBUUID { get }
     var method: RequestMethod { get }
     var value: Data? { get }
     var characteristicUUID: CBUUID? { get }
-    var charactertic: CBMutableCharacteristic { get }
+    var characteristic: CBMutableCharacteristic { get }
 }
 
-
 extension Communicable {
+
     public var method: RequestMethod {
         return .get(isNotified: false)
     }
-    
+
     public var value: Data? {
         return nil
     }
-    
+
     public var hashValue: Int {
-        guard let charactersicitcUUID: CBUUID = self.characteristicUUID else {
-            fatalError("ERRO")
+        guard let characteristicUUID: CBUUID = self.characteristicUUID else {
+            fatalError("*** LogError: characteristicUUID must be defined for Communicable. ***")
         }
-        return charactersicitcUUID.hash
+        return characteristicUUID.hash
+    }
+
+    public var characteristic: CBMutableCharacteristic {
+        return CBMutableCharacteristic(type: self.characteristicUUID!, properties: self.method.properties, value: nil, permissions: self.method.permissions)
     }
 }
 
-public func == <T: Communicable>(lhs: T, rhs:T) -> Bool {
-    return lhs.characteristicUUID == rhs.charactertic
+public func == <T: Communicable>(lhs: T, rhs: T) -> Bool {
+    return lhs.characteristicUUID == rhs.characteristicUUID
 }
