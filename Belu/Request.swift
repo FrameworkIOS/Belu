308bc7962dabd1710823461085d7e42f975736a4
krishpranav <krisna.pranav@gmail.com>
2023-07-09 17:47:03 +0530

    Belu: request

--- Belu/Request.swift
+++ Belu/Request.swift
@@ -9,18 +9,31 @@ import Foundation
 import CoreBluetooth
 
 public class Request: Communicable {
+
     public typealias ResponseHandler = ((CBPeripheral, CBCharacteristic, Error?) -> Void)
-    public let serviceUUID: CBUUID
-    public let method: RequestMethod
-    public let characteristicUUID: CBUUID?
-    public let characteristic: CBMutableCharacteristic
-    public let options: [String, Any]
-    public var value: Data?
-    public var response: ResponseHandler?
     
-    init(serviceUUID: serviceUUID, method: method, characteristic: characteristic) {
-        self.serviceUUID = serviceUUID
-        self.method = method
-        self.characteristic = characteristic
+    public let serviceUUID: CBUUID
+    
+    public let method: RequestMethod
+
+    public let characteristicUUID: CBUUID?
+
+    public let characteristic: CBMutableCharacteristic
+
+    public var options: [String: Any]?
+
+    public var value: Data?
+
+    public var response: ResponseHandler?
+
+    public init<T: Communicable>(communication: T, response: @escaping ResponseHandler) {
+        self.serviceUUID = communication.serviceUUID
+        self.method = communication.method
+        self.response = response
+        self.characteristicUUID = communication.characteristicUUID
+        self.characteristic = CBMutableCharacteristic(type: communication.characteristicUUID!,
+                                                      properties: method.properties,
+                                                      value: communication.value,
+                                                      permissions: method.permissions)
     }
 }
